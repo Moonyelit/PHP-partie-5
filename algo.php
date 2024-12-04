7d2e1ab7d2e3d4e9a11ec5fe6633f505681e53d8
moonye@live.fr <moonye@live.fr>
2024-12-04 14:21:27 +0100

    EXO 5

--- algo.php
+++ algo.php
@@ -5,7 +5,7 @@ echo "<br>";
 
 $mois = array ("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
 print_r($mois);
-"<br>";
+echo "<br>";
 var_dump($mois);
 
 // EXERCICE 2
@@ -33,3 +33,27 @@ $mois[7] = "août";
 echo $mois[7];
 
 
+// EXERCICE 5
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 5";
+echo "<br>";
+
+$HautsFrance = array(
+02 => "Aisne",
+59 => "Nord",
+60 => "Oise",
+62 => "Pas-de-Calais",
+80 => "Somme",
+);
+
+print_r($HautsFrance);
+echo "<br>";
+var_dump($HautsFrance);
+
+
+// EXERCICE 6
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 6";
+echo "<br>";
