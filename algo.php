2ddeb7e7abe86d50f59a5714a9dafef2ff4e672b
moonye@live.fr <moonye@live.fr>
2024-12-04 14:09:42 +0100

    Exo 2 à 4

--- algo.php
+++ algo.php
@@ -3,8 +3,33 @@
 echo "EXERCICE 1";
 echo "<br>";
 
-$exo1var1 = array ("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
-print_r($exo1var1);
+$mois = array ("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
+print_r($mois);
 "<br>";
-var_dump($exo1var1);
+var_dump($mois);
+
+// EXERCICE 2
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 2";
+echo "<br>";
+
+echo $mois[2];
+
+// EXERCICE 3
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 3";
+echo "<br>";
+echo $mois[5];
+
+// EXERCICE 4
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 4";
+echo "<br>";
+
+$mois[7] = "août";
+echo $mois[7];
+
 
