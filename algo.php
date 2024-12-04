678641a52fa38544bf31d8e107f0d46b4592f0c7
moonye@live.fr <moonye@live.fr>
2024-12-04 15:04:10 +0100

    EXO finis

--- algo.php
+++ algo.php
@@ -3,7 +3,7 @@
 echo "EXERCICE 1";
 echo "<br>";
 
-$mois = array ("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
+$mois = array("janvier", "février", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "décembre");
 print_r($mois);
 echo "<br>";
 var_dump($mois);
@@ -40,11 +40,11 @@ echo "EXERCICE 5";
 echo "<br>";
 
 $HautsFrance = array(
-02 => "Aisne",
-59 => "Nord",
-60 => "Oise",
-62 => "Pas-de-Calais",
-80 => "Somme",
+    02 => "Aisne",
+    59 => "Nord",
+    60 => "Oise",
+    62 => "Pas-de-Calais",
+    80 => "Somme",
 );
 
 print_r($HautsFrance);
@@ -57,3 +57,52 @@ echo "<br>";
 echo "<br>";
 echo "EXERCICE 6";
 echo "<br>";
+
+echo $HautsFrance[59];
+
+// EXERCICE 7
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 7";
+echo "<br>";
+
+$HautsFrance[51] = "Reims";
+
+print_r($HautsFrance);
+echo "<br>";
+var_dump($HautsFrance);
+
+
+// EXERCICE 8
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 8";
+echo "<br>";
+
+foreach ($mois as &$value) {
+    echo $value;
+    echo "<br>";
+};
+
+
+// EXERCICE 9
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 9";
+echo "<br>";
+
+foreach ($HautsFrance as &$value) {
+    echo $value;
+    echo "<br>";
+};
+
+
+// EXERCICE 10
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 10";
+echo "<br>";
+
+foreach ($HautsFrance as $numero => $nom) {
+    echo "Le département {$nom} a le numéro {$numero}<br>";
+}
